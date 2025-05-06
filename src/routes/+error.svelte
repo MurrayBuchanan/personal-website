9a4370becf92d88fa98fe2ab5e127be0c3798cfe
Murray Buchanan <murray.buchanan.2022@uni.strath.ac.uk>
2025-05-06 17:52:57 +0100

    Lottle, SEO and 404 Handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { useAnimate } from './animation/animation';
+    import Container from './components/container.svelte';
+    import Footer from './components/footer.svelte';
+    import Heading from './components/section-heading.svelte';
+    import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
+
+    export let error;
+</script>
+  
+<svelte:head>
+    <title>Murray Buchanan | Not Found</title>
+</svelte:head>
+  
+
+
+<body class="bg-primary-light dark:bg-primary-dark" use:useAnimate={0.1}>
+    <Container>
+        <Heading
+            title="{error?.message || 'Page not found'}"
+            desc="The page you’re looking for doesn’t exist." 
+        />       
+
+        <DotLottieSvelte src="static/assets/Animation.lottie" loop autoplay />
+
+        <Footer />
+
+    </Container>
+</body>
