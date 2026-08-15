4b199e0261ee5433de5a657037e671ef3d5672f8
Murray Buchanan <murray.buchanan.2022@uni.strath.ac.uk>
2026-08-15 12:58:05 +0700

    Deliberate width

--- src/lib/components/layout/Container.svelte
+++ src/lib/components/layout/Container.svelte
@@ -6,7 +6,7 @@
     class="flex w-full flex-1 flex-col items-center px-3 sm:px-6 md:px-10 lg:px-16 xl:px-24 2xl:px-32"
 >
     <div
-        class="mb-6 flex w-full flex-1 flex-col rounded-3xl px-4 pt-1 sm:pt-2 lg:w-1/3 {custom}"
+        class="mb-6 flex w-full flex-1 flex-col rounded-3xl px-4 pt-1 sm:pt-2 lg:w-full lg:max-w-[34rem] xl:max-w-[40rem] 2xl:max-w-[46rem] {custom}"
     >
         <slot></slot>
     </div>
